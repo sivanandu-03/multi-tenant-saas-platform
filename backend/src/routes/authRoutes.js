274d4a8c24892131dc04a0dc9f6c330603d1f8d7
Sivanandu-03 <sivanall03@gmail.com>
2025-12-26 14:47:57 +0530

    updating Auth  routes file

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
+
+
 router.post('/register-tenant', authController.registerTenant);
 router.post('/register', authController.register); // Keep for adding users to existing tenant
 router.post('/login', authController.login);
